1a910a973d7c85213f15d2220ab2adc778b49b8e
Adrieaneeuu <111805721+Adrieaneeuu@users.noreply.github.com>
2024-04-23 00:03:01 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,22 +1,44 @@
-package com.example;
-
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import net.minecraft.util.hitbox.BoundingBox;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    private static final KeyBinding keyAltZ = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding("key.examplemod.alt_z", GLFW.GLFW_KEY_Z, "category.examplemod")
+    );
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    private static final KeyBinding keyAltX = KeyBindingHelper.registerKeyBinding(
+        new KeyBinding("key.examplemod.alt_x", GLFW.GLFW_KEY_X, "category.examplemod")
+    );
+
+    @Override
+    public void onInitialize() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (keyAltZ.isPressed() && InputUtil.isKeyDown(GLFW.GLFW_KEY_LEFT_ALT)) {
+                adjustHitbox(client, -5);
+            }
+
+            if (keyAltX.isPressed() && InputUtil.isKeyDown(GLFW.GLFW_KEY_LEFT_ALT)) {
+                adjustHitbox(client, 5);
+            }
+        });
+    }
+
+    private void adjustHitbox(MinecraftClient client, int percent) {
+        if (client.world != null) {
+            for (PlayerEntity player : client.world.getPlayers()) {
+                double scale = (100 + percent) / 100.0;
+                BoundingBox newHitbox = player.getBoundingBox().expand(scale);
+                player.setBoundingBox(newHitbox);
+                player.sendMessage(Text.of("Hitbox changed by " + percent + "%"), false);
+            }
+        }
+    }
 }
