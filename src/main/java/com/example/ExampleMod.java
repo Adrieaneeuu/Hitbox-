c061ce9abb0cf318cb9e42bb9104eaa75dddac61
Adrieaneeuu <111805721+Adrieaneeuu@users.noreply.github.com>
2024-04-23 01:44:25 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,44 +1,67 @@
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
+package com.yourdomain.hitboxchangermod;
+
+import com.google.common.collect.ImmutableSet;
+import net.fabricmc.fabric.api.ClientCommand;
+import net.fabricmc.fabric.api.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
-import net.minecraft.util.hitbox.BoundingBox;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.Keyboard;
 
-public class ExampleMod implements ModInitializer {
+import java.util.Set;
 
-    private static final KeyBinding keyAltZ = KeyBindingHelper.registerKeyBinding(
-        new KeyBinding("key.examplemod.alt_z", GLFW.GLFW_KEY_Z, "category.examplemod")
-    );
+public class HitboxChangerMod implements ClientCommand {
 
-    private static final KeyBinding keyAltX = KeyBindingHelper.registerKeyBinding(
-        new KeyBinding("key.examplemod.alt_x", GLFW.GLFW_KEY_X, "category.examplemod")
-    );
+    private static final float HITBOX_CHANGE_AMOUNT = 0.05f;
 
     @Override
-    public void onInitialize() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (keyAltZ.isPressed() && InputUtil.isKeyDown(GLFW.GLFW_KEY_LEFT_ALT)) {
-                adjustHitbox(client, -5);
-            }
-
-            if (keyAltX.isPressed() && InputUtil.isKeyDown(GLFW.GLFW_KEY_LEFT_ALT)) {
-                adjustHitbox(client, 5);
-            }
-        });
+    public Set<String> getAliases() {
+        return ImmutableSet.of("decreasehitbox", "increasehitbox");
     }
 
-    private void adjustHitbox(MinecraftClient client, int percent) {
-        if (client.world != null) {
-            for (PlayerEntity player : client.world.getPlayers()) {
-                double scale = (100 + percent) / 100.0;
-                BoundingBox newHitbox = player.getBoundingBox().expand(scale);
-                player.setBoundingBox(newHitbox);
-                player.sendMessage(Text.of("Hitbox changed by " + percent + "%"), false);
+    @Override
+    public void execute(MinecraftClient client, String message) {
+        // This method is not used in this implementation
+    }
+
+    public void decreaseHitbox() {
+        changeHitbox(-HITBOX_CHANGE_AMOUNT);
+    }
+
+    public void increaseHitbox() {
+        changeHitbox(HITBOX_CHANGE_AMOUNT);
+    }
+
+    private void changeHitbox(float changeAmount) {
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if (player != null) {
+            for (Entity entity : MinecraftClient.getInstance().world.getEntities()) {
+                if (entity != player) {
+                    float currentHitbox = entity.getBoundingBox().getRadiusX();
+                    entity.setBoundingBox(entity.getBoundingBox().withNewDimensions(
+                            entity.getBoundingBox().getWidth() + changeAmount,
+                            entity.getBoundingBox().getHeight(),
+                            entity.getBoundingBox().getLength() + changeAmount));
+                    // Send message to chat (optional)
+                    MinecraftClient.getInstance().player.sendMessage(LiteralText.of("Хитбокс изменен на " + (changeAmount * 100) + "%"), true);
+                }
             }
+        }
+    }
+
+    public void register() {
+        ClientCommandRegistrationCallback.EVENT.register(this);
+        MinecraftClient.getInstance().getInput().addKeyListener(this::handleKeypress);
+    }
+
+    @Override
+    public void handleKeypress(KeyboardInputEvent event) {
+        if (event.isKeyPressed(Keyboard.KEY_ALT) && event.isKeyPressed(Keyboard.KEY_Z)) {
+            decreaseHitbox();
+        } else if (event.isKeyPressed(Keyboard.KEY_ALT) && event.isKeyPressed(Keyboard.KEY_X)) {
+            increaseHitbox();
         }
     }
 }
